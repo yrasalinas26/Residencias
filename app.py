4dc6cca417506848ae7d85ec14e2a66fd074d5e0
yrasalinas26 <yrasalinas26@gmail.com>
2026-08-14 21:34:27 -0400

    Refactor app.py for database connection and UI

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # CONEXIÓN A LA BASE DE DATOS (SUPABASE / POSTGRESQL)
 # ---------------------------------------------------------
 # Conexión directa a Supabase (Transaction Pooler)
-DB_URL = "postgresql://postgres.xxxx:Nispero2606.@aws-0-sa-east-1.pooler.supabase.com:6543/postgres"
+DB_URL = "postgresql://postgres.psathqqomnsvzhytvbsu:Nispero2606.@aws-0-ca-central-1.pooler.supabase.com:6543/postgres"
 
 @st.cache_resource
 def get_db_engine():
