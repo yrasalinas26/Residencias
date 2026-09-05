147343e487944cb3c0788f85755e85075eccb16c
yrasalinas26 <yrasalinas26@gmail.com>
2026-09-05 12:21:12 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1307,13 +1307,13 @@ elif st.session_state.rol_logueado == "admin":
     except Exception as e:
       st.error(f"Error gestionando unidades: {e}")
 
- with t7:
-  st.subheader("🚨 Recibos y Envíos a WhatsApp")
-  mes_recibo_gral = st.text_input(
-      "Periodo del Recibo (AAAA-MM):",
-      value=obtener_mes_anterior(),
-      key="input_mes_recibo_gen",
-  )
+    with t7:
+      st.subheader("🚨 Recibos y Envíos a WhatsApp")
+      mes_recibo_gral = st.text_input(
+          "Periodo del Recibo (AAAA-MM):",
+          value=obtener_mes_anterior(),
+          key="input_mes_recibo_gen",
+          )
 
   try:
     with engine.connect() as conn:
