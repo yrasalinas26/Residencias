da08fd07743e7fc52cf901a92b5e58934ea4a013
yrasalinas26 <yrasalinas26@gmail.com>
2026-09-04 23:20:00 -0400

    Change greeting from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -887,7 +887,7 @@ t1, t2, t3, t4, t5, t6, t7, t8 = st.tabs([
     "⚙️ Datos Edificio",
 ])
 
-  with t1:
+with t1:
     st.subheader("➕ Cargar Nuevo Gasto Común")
     with st.form("form_gasto"):
       col_g1, col_g2 = st.columns(2)
